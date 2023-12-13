32a0018b16293501acead9b8d38903458e6cffb4
Francisco Reis <franc.mdreis@gmail.com>
2023-12-13 16:25:22 +0000

    Updated Player

--- src/Models/Player.cs
+++ src/Models/Player.cs
@@ -2,63 +2,64 @@
    
     internal class Player {
     
-    private string _name;
-    private int _victories;
-    private int _symbol;
-    private int _color;
-    private int _TotalGamesPlayed;
-   private int _draws;
-   private int _loses;
+        private string _name;
+        private int _victories;
+        private int _symbol;
+        private int _color;
+        private int _totalGamesPlayed; 
+        private int _draws;
+        private int _loses;
 
-     public string Name
-     {
-        get {return _name;}
-        get {_name = value;}
-     }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; } 
+        }
 
-     public int Color
-     {
-        get {return _color;}
-        set {_color = value;}
-     }
+        public int Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
 
-      public int Symbol
-      {
-         get {return _symbol;}
-         set {_symbol = value;}
-      }
+        public int Symbol
+        {
+            get { return _symbol; }
+            set { _symbol = value; }
+        }
 
-     public int Victories
-     {
-        get {return _victories;}
-        set {_victories = value;}
-     }
+        public int Victories
+        {
+            get { return _victories; }
+            set { _victories = value; }
+        }
 
+        public int TotalGamesPlayed
+        {
+            get { return _totalGamesPlayed; }
+            set { _totalGamesPlayed = value; }
+        }
 
-     public int TotalGamesPlayed{
-        get {return _TotalGamesPlayed;}
-        set {_TotalGamesPlayed = value;}
-     }
+        public int Draws
+        {
+            get { return _draws; }
+            set { _draws = value; }
+        }
 
-     public int Draws
-     {
-        get {return _draws;}
-        set {_draws = value;}
-     }
+        public int Loses
+        {
+            get { return _loses; }
+            set { _loses = value; }
+        }
 
-     public int Loses
-     {
-        get {return _loses;}
-        set {_loses = value;}
-     }
-
-     public Player(string name)
-     {
-        _name = name;
-        victories = 0;
-        _gamesPlayed = 0;
-     }
-
-    
+        
+        public Player(string name)
+        {
+            _name = name;
+            _victories = 0;
+            _totalGamesPlayed = 0; 
+            _draws = 0;
+            _loses = 0;
+        }
     }
 }
