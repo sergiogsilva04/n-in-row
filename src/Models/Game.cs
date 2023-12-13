bcad26019df916a13bc2cc3fdb7c8d41ff94389e
RicardoMartins <ricardofilipe.martins@my.istec.pt>
2023-12-13 17:10:28 +0000

    game

--- src/Models/Game.cs
+++ src/Models/Game.cs
@@ -1,4 +1,25 @@
 ﻿namespace n_in_row.src.Models {
-    internal class Game {
+    internal class Game 
+    {
+        public int VictoryLength { get; private set; }
+
+        public GameBoard board { get; private set; }
+
+        public Game(int victoryLength, GameBoard board)
+        {
+            VictoryLength = victoryLength;
+            this.board = board;
+        }
+
+        public void StartGame() { }
+
+        public void GameDetails() { }
+
+        public void Forfeit() { }
+
+        public void Play() { }
+
+        public void ShowGameBoard() { }
+
     }
 }
