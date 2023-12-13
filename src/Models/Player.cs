7a6667d17ad7745ed2acabe972980ecedbff173b
Francisco Reis <franc.mdreis@gmail.com>
2023-12-13 15:58:18 +0000

    Started creating PLayer Class

--- src/Models/Player.cs
+++ src/Models/Player.cs
@@ -1,4 +1,35 @@
 ﻿namespace n_in_row.src.Models {
     internal class Player {
+    
+    private string _name;
+    private int _victories;
+    private int _gamesPlayed;
+
+     public string Name
+     {
+        get {return _name;}
+        get {_name = value;}
+     }
+
+     public int _victories
+     {
+        get {return _victories;}
+        set {_victories = value;}
+     }
+
+
+     public int GamesPlayed{
+        get {return _gamesPlayed;}
+        set {_gamesPlayed = value;}
+     }
+
+     public Player(string name)
+     {
+        _name = name;
+        victories = 0;
+        _gamesPlayed = 0;
+     }
+
+    
     }
 }
