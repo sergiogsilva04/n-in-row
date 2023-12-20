8d9d500a036edacff7ae761a2e69e77991758035
sergiogsilva04 <sergiogsilva14@gmail.com>
2023-12-20 19:50:30 +0000

    Finished Play() method based on the 4 in a row game; Finished ShowGameBoard();
    Needs to be done for the N in a row yet.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace n_in_row {
         static void Main() {
             Console.OutputEncoding = Encoding.UTF8;
 
-            Console.WriteLine("Hello, World!");
+            Game currentGame = new Game(
+                victoryLength: 4,
+                new GameBoard(lines: 4, columns: 4),
+                player1: new Player("Sérgio Silva", "S", "#fff"),
+                player2: new Player("Francisco", "F", "#000")
+            );
+
+            do {
+                Console.WriteLine($"É a vez do jogador: {currentGame.CurrentPlayer.Name}");
+                Console.Write("Qual é a coluna que vai colocar a peça? ");
+                int column = int.Parse(Console.ReadLine()!);
+
+                while (column < 1 || column > currentGame.Board.Columns) {
+                    Console.WriteLine($"Coluna inválida. Tem que ser um número entre 1 e {currentGame.Board.Columns}");
+
+                    Console.Write("\nQual é a coluna que vai colocar a peça? ");
+                    column = int.Parse(Console.ReadLine()!);
+                }
+
+                currentGame.Play(column - 1);
+
+                Console.WriteLine();
+
+            } while (!currentGame.IsGameFinished);
         }
     }
 }
