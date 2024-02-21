0d7c8d17fe136a6df2ded4a7c8efc7feac0b4bc0
sergiogsilva04 <sergiogsilva14@gmail.com>
2024-02-21 15:43:17 +0000

    Changed List to Dictionary

--- src/Controllers/PlayerController.cs
+++ src/Controllers/PlayerController.cs
@@ -3,32 +3,26 @@
 namespace n_in_row.src.Controllers
 {
     internal class PlayerController() {
-        public List<Player> PlayerList { get; private set; } = [];
+        public Dictionary<string, Player> PlayerDictionary { get; private set; } = [];
 
-        public void AddPlayer(Player newPlayer)
-        {
-            if (PlayerList.Contains(newPlayer))
-            {
-                Console.WriteLine("The player is already in the list");
+        public void AddPlayer(Player player) {
+            if (!PlayerDictionary.ContainsKey(player.Name)) {
+                Console.WriteLine($"Player '{player.Name}' is already in the dictionary.");
 
                 return;
             }
 
-            PlayerList.Add(newPlayer);
+            PlayerDictionary.Add(player.Name, player);
 
-            Console.WriteLine($"Player {newPlayer} added to the list.");
+            Console.WriteLine($"Player {player.Name} added to the dictionary.");
         }
 
-        public void RemovePlayer(Player player)
-        {
-            if (PlayerList.Contains(player))
-            {
-                PlayerList.Remove(player);
-                Console.WriteLine($"Player {player.Name} removed from the list.");
-            }
-            else
-            {
-                Console.WriteLine($"Player {player.Name} not found in the list.");
+        public void RemovePlayer(Player player) {
+            if (PlayerDictionary.ContainsKey(player.Name)) {
+                PlayerDictionary.Remove(player.Name);
+                Console.WriteLine($"Player {player.Name} removed from the dictionary.");
+            } else {
+                Console.WriteLine($"Player {player.Name} not found in the dictionary.");
             }
         }
     }
